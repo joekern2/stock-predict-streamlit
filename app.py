11b8edc1c042bd793b90da2960dabbaca9e1f625
joekern2 <116761823+joekern2@users.noreply.github.com>
2022-12-11 15:56:27 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import neighbors, datasets, svm, linear_model, tree
+from sklearn import neighbors #, datasets, svm, linear_model, tree
 from sklearn.model_selection import (cross_val_score, train_test_split, 
 StratifiedKFold)
 from sklearn.metrics import (confusion_matrix, precision_recall_curve, 
