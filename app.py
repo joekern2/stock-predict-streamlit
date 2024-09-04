487904b77a2997e090ec6251034004943c98e016
joekern2 <116761823+joekern2@users.noreply.github.com>
2024-09-04 18:15:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -98,8 +98,7 @@ def run():
     
     if ticker == 'Tesla':
         # TSLA - Tesla
-        loaded_model = pd.read_pickle(open('tsla_model.sav', 'rb'))
-        #loaded_model = pickle.load(open('tsla_model.sav', 'rb'))
+        loaded_model = pickle.load(open('tsla_model.sav', 'rb'))
         result = loaded_model.predict(todx)[0]
     elif ticker == 'Apple':
         # AAPL - Apple
